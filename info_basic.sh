f257700f0fbbc5338f764227958b8aabbe57afe0
Jack Edwards <s3727853@student.rmit.edu.au>
2019-10-30 13:00:24 +1100

    Implemented running proccesses feature and updated some minor layout

--- info_basic.sh
+++ info_basic.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
+case $1 in
+	1)
+		#Print free memory
+		echo "Occupied and free memory in MB"
+		free -m
+		;;
+	2)
+		#Print free disk space
+		echo -e "Occupied and free disk space\n"
+	       	df -hx squashfs -x tmpfs --total
+		;;
+	3)
+		#Print connection info
+		echo -e "System Connection Info\n"
+		ifconfig -a | grep -v "RX\|TX"
+		;;
+	4)
+		#Print system load average
+		echo -e "System Load Average\n"
+		w
+		;;
+esac
 
--- info_post.sh
+++ info_post.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+while getopts "abcde" c
+do
+	case $c in
+		a) printf "Number of CPU cores on this system is: "
+		       	nproc ;;
+		b) echo "Current process priority is"
+			p=$$
+			
+			ps -o pri -p $p
+	  		;;
+		c) user="$(whoami)"
+			printf "Number of processes running under current user "$user" is: " 
+			ps -U "$user" | wc -l
+			;;
+		d) echo "d" ;;
+		e) echo "e" ;;
+	esac
+done
