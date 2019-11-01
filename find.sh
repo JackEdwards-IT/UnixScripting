9c4225221b66d666c46054a204b7d9880c1ffa78
Jack Edwards <s3727853@student.rmit.edu.au>
2019-11-01 11:47:44 +1100

    Started implementation of the find script

--- find.sh
+++ find.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+printf "Welcome to the find script\n"
+printf "Please enter the directory the search should begin from\n"
+read -p ":" searchdir
+
+echo $searchdir
+
+#Selection menu for thing to search for, simple while  case loop with input validationt	
+printf "Please select the thing you are searching for\n"
+printf "1 - path\n2 - type\n3 - group\n4 - fstype\n"
+
+while read -p "enter a number beetween 1 and 4:" selection; do
+	case "$selection" in
+	
+		1) searchitem="path"; break ;;
+		2) searchitem="type"; break ;;
+		3) searchitem="group"; break ;;
+		4) searchitem="fstype"; break ;;
+		*)echo "Incorect input, please enter a option from 1 to 4" ;;
+	esac
+done
+
+echo $searchitem
